0384f6e8690b3c6e73b784c538d255339b5b1344
Aryan Jain <aryanj@mit.edu>
2026-01-03 19:02:17 -0800

    Add TikTok social link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -114,6 +114,9 @@ export default function Footer() {
               <a href="#" className="text-stone-600 hover:text-black">
                 Pinterest
               </a>
+              <a href="#" className="text-stone-600 hover:text-black">
+                TikTok
+              </a>
             </div>
           </div>
         </div>
