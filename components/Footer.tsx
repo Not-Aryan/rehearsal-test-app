085925ab1bfec4756be64772f4ab3246c492b456
Aryan Jain <aryanj@mit.edu>
2026-01-03 19:10:48 -0800

    Add YouTube social link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -114,6 +114,9 @@ export default function Footer() {
               <a href="#" className="text-stone-600 hover:text-black">
                 Pinterest
               </a>
+              <a href="#" className="text-stone-600 hover:text-black">
+                YouTube
+              </a>
             </div>
           </div>
         </div>
